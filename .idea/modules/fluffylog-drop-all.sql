2416b2ed29ec5ec95f34be5b12afc58df185cf78
Daniel Watson <daniel@splendidbits.co>
2018-03-25 11:44:25 -0400

    Remove idea files from repo.

--- .idea/modules/fluffylog-drop-all.sql
+++
@@ -1,9 +0,0 @@
-alter table if exists fluffylog.entries drop constraint if exists fk_entries_application_id;
-drop index if exists ix_entries_application_id;
-
-drop table if exists fluffylog.applications cascade;
-drop sequence if exists log_application_id_seq;
-
-drop table if exists fluffylog.entries cascade;
-drop sequence if exists log_entry_gen;
-
